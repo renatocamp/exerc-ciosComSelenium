0528a421d9c22df0faafa531c0a09f1f0b4e6969
Renato <86419088+renatocamp@users.noreply.github.com>
2022-03-23 10:20:50 -0300

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -10,21 +10,26 @@ public class HomePage {
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
-
+    
+    //Método para clicar no link para fazer login
     public LoginPage clickFormAuthentication(){
         clickLink("Form Authentication");
         return new LoginPage(driver);
     }
-
+    
+    //Método para interação com Menus DropDown
     public DropdownPage clickDropDown(){
         clickLink("dropdown");
         return new DropdownPage(driver);
     }
-
+    
+    //Método para clicar no link "Hover" da Página em questão
     public HoversPage clickHoverPage(){
         clickLink("Hovers");
         return new HoversPage(driver);
     }
+    
+    //Método que irá retornar o link do WebElement Desejado
     private void clickLink(String linkText){
         driver.findElement(By.linkText(linkText)).click();
     }
